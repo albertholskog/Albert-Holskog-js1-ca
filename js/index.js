ddbd2bde6ba9d0ab90dad96a296be6db4a06a44a
albertholskog <91052944+albertholskog@users.noreply.github.com>
2022-02-17 14:38:36 +0100

    api

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// API har ingen key
+const url = "http://universities.hipolabs.com/search?country=norway";
+
+const uniContainer = document.querySelector(".container");
+
+async function uniAPI() {
+    try {
+        const response = await fetch(url);
+        const uniResult = await response.json();
+        console.log(uniResult);
+        uniContainer.innerHTML = "";
+        for (let i = 0; i < uniResult.length; i++) {
+            if (i === 12) {
+                break;
+            }
+            uniContainer.innerHTML += `<a href="/details.html"><h2>${uniResult[i].name}</h2></a>`;
+            console.log(uniResult[i].name)
+        }
+    } catch (err) {
+        console.log(err);
+        uniContainer.innerHTML = `<div><h3>error 404</h3></div>`;
+    }
+}
+uniAPI();
